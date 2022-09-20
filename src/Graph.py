0628bf27f92c05ecce82e412c8d8ffd0ed273275
Jad Haytaoglu <jadhaytaoglu@hotmail.com>
2022-09-20 12:49:45 -0400

    added some methods to Graph class and implemented Dijkstras

--- src/Graph.py
+++ src/Graph.py
@@ -18,13 +18,29 @@ class Graph:
             self.m_adj_list[node2].add((node1, weight))
 
     def get_nodes(self):
-        return self.m_nodes
+        return self.m_num_of_nodes
+
+    def value(self, node1, node2):
+        "Returns the value of an edge between two nodes."
+        for tuple in self.m_adj_list.get(node1):
+            if tuple[0] == node2:
+                return tuple[1]
+
+    def get_outgoing_edges(self, node):
+        "Returns the neighbors of a node."
+        connections = []
+        # append all neighbours of node
+        for tuple in self.m_adj_list.get(node):
+            connections.append(tuple[0])
+
+        return connections
 
     def print_adj_list(self):
-        print(self.m_adj_list.keys())
+        # print(self.m_adj_list.get(1))
+        # print("adjacency list: \t", self.m_adj_list)
         for key in self.m_adj_list.keys():
-            print("hello", key)
-            # print("node", key, ": ", self.m_adj_list[key])
+            print("node", key, ": ", self.m_adj_list[key])
+        print("end adj list \n")
 
 # Source: https://stackabuse.com/courses/graphs-in-python-theory-and-implementation/lessons/representing-graphs-in-code/
 
@@ -40,21 +56,19 @@ class Graph:
 # graph.add_edge(4, 3, 11)
 # graph.print_adj_list()
 
-with open('_dataset/london.stations.csv') as file:
-    stations = csv.reader(file)
+# with open('_dataset/london.stations.csv') as file:
+#     stations = csv.reader(file)
 
-    # row count other than first line of headers is number of stations
-    numStations = sum(1 for row in stations)-1
+#     # row count other than first line of headers is number of stations
+#     numStations = sum(1 for row in stations)+1
 
-    graph = Graph(numStations)
+#     graph = Graph(numStations)
 
-    with open('_dataset/london.connections.csv') as file2:
-        connections = csv.reader(file2)
-        for fileLine in connections:
-            print(fileLine)
-            if connections.line_num != 1:
-                # print(fileLine)
-                graph.add_edge(int(fileLine[0]), int(
-                    fileLine[1]), int(fileLine[3]))
+#     with open('_dataset/london.connections.csv') as file2:
+#         connections = csv.reader(file2)
+#         for fileLine in connections:
+#             if connections.line_num != 1:
+#                 graph.add_edge(int(fileLine[0]), int(
+#                     fileLine[1]), int(fileLine[3]))
 
-    graph.print_adj_list()
+#     graph.print_adj_list()
--- src/Itinerary.py
+++ src/Itinerary.py
@@ -1,5 +1,6 @@
 from Graph import Graph
 import sys
+import csv
 
 
 class Itinerary:
@@ -9,31 +10,65 @@ class Itinerary:
         self.station2 = station2
 
     def rankPaths(self, path1, path2):
+        pass
         # if length(path1) == length(path2):
         #   whichever has less transfers
         # otherwise: if shortestPath(path1,path2) == path1: return path1 else return path2
 
-    def dijkstra(self, graph, start_node) -> list[int]:
-        unvisited_nodes = list(graph.get_nodes())
+    def numTransfers(self, other):
+        TODO
+        pass
 
-        shortest_path = {}
-        previous_nodes = {}
-        # We'll use max_value to initialize the "infinity" value of the unvisited nodes
-        max_value = sys.maxsize
-        for node in unvisited_nodes:
-            shortest_path[node] = max_value
-        # However, we initialize the starting node's value with 0
-        shortest_path[start_node] = 0
+    def getItinerary(self):
+        '''
+        get shortest path and rank to get the number 1 best path
+        '''
+        TODO
+        pass
 
-        while unvisited_nodes:
-            current_min_node = None
-            for node in unvisited_nodes:  # Iterate over the nodes
-                if current_min_node == None:
-                    current_min_node = node
-                elif shortest_path[node] < shortest_path[current_min_node]:
-                    current_min_node = node
 
-                # The code block below retrieves the current node's neighbors and updates their distances
+def print_result(previous_nodes, shortest_path, start_node, target_node):
+    path = []
+    node = target_node
+
+    while node != start_node:
+        path.append(node)
+        node = previous_nodes[node]
+
+    # Add the start node manually
+    path.append(start_node)
+
+    print("We found the following best path with a value of {}.".format(
+        shortest_path[target_node]))
+    for i in range(len(path)-1, 0, -1):
+        print(path[i], "->", end=' ')
+    print(path[0])
+    # print(" -> ".join(reversed(str(path))))
+
+
+def dijkstra(graph, start_node):
+    # Source: https://www.udacity.com/blog/2021/10/implementing-dijkstras-algorithm-in-python.html
+    unvisited_nodes = list(range(graph.get_nodes()))
+
+    shortest_path = {}
+    previous_nodes = {}
+    # We'll use max_value to initialize the "infinity" value of the unvisited nodes
+    max_value = sys.maxsize
+    for node in unvisited_nodes:
+        shortest_path[node] = max_value
+    # However, we initialize the starting node's value with 0
+    shortest_path[start_node] = 0
+
+    print(unvisited_nodes)
+    while unvisited_nodes:
+        current_min_node = None
+        for node in unvisited_nodes:  # Iterate over the nodes
+            if current_min_node == None:
+                current_min_node = node
+            elif shortest_path[node] < shortest_path[current_min_node]:
+                current_min_node = node
+
+        # The code block below retrieves the current node's neighbors and updates their distances
         neighbors = graph.get_outgoing_edges(current_min_node)
         for neighbor in neighbors:
             tentative_value = shortest_path[current_min_node] + \
@@ -43,25 +78,43 @@ class Itinerary:
                 # We also update the best path to the current node
                 previous_nodes[neighbor] = current_min_node
 
-    def numTransfers(self, other):
-        TODO
-        pass
+        unvisited_nodes.remove(current_min_node)
 
-    def getItinierary(self):
-        '''
-        get shortest path and rank to get the number 1 best path
-        '''
-        TODO
-        pass
+    return previous_nodes, shortest_path
 
 
-itinerary1 = Itinerary(Graph, station1, station2)
-itinerary1.getItinerary()
+def test():
+    with open('_dataset/london.stations.csv') as file:
+        stations = csv.reader(file)
 
-'''
-node 0: {(10,2),(100,4),...}
-node 1: {(120,2),(101,3),...}
-.
-.
-.
-'''
+        # row count other than first line of headers is number of stations
+        numStations = sum(1 for row in stations)+1
+
+        graph = Graph(numStations)
+
+        with open('_dataset/london.connections.csv') as file2:
+            connections = csv.reader(file2)
+            for fileLine in connections:
+                if connections.line_num != 1:
+                    graph.add_edge(int(fileLine[0]), int(
+                        fileLine[1]), int(fileLine[3]))
+
+    # graph.print_adj_list()
+
+    # graph = Graph(10)
+    # graph.add_edge(1, 2, 3)
+    # graph.add_edge(5, 3, 2)
+    # graph.add_edge(8, 4, 6)
+    # graph.add_edge(8, 7, 2)
+    # graph.add_edge(9, 7, 1)
+    # graph.add_edge(9, 8, 3)
+    # graph.add_edge(6, 0, 2)
+
+    # graph.print_adj_list()
+
+    previous_nodes, shortest_path = dijkstra(graph, 1)
+
+    print_result(previous_nodes, shortest_path, 1, 286)
+
+
+test()
