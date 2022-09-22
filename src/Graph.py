94dc741ade8a1c9403079bf0ef2154ff121e39de
Evan Placenis <placenie@mcmaster.ca>
2022-09-21 20:24:14 -0400

    added a method to find degree of nodes and made graphbuilding a class

--- src/Graph.py
+++ src/Graph.py
@@ -71,30 +71,45 @@ class Graph:
 
 
 #------------------creating graph ---------------------------------
-with open('_dataset/london.stations.csv') as file:
-    stations = csv.reader(file)
+class BuildGraph:
 
-    # row count other than first line of headers is number of stations
-    numStations = sum(1 for row in stations)+1
+    def __init__(self, file, file2):
+        self.file = file
+        self.file2 = file2
+        
 
-    graph = Graph(numStations)
 
-    #Have to open it again for some reason
-    with open('_dataset/london.stations.csv') as filee:
-        stationss = csv.reader(filee)
-        for fileLine in stationss:
-            if stationss.line_num != 1:
-                graph.add_node_dist(float(fileLine[0]), float(fileLine[1]), float(fileLine[2]))
+    def build_graph(self):
+            # row count other than first line of headers is number of stations
+        with open('_dataset/london.stations.csv') as self.file:
+            stations = csv.reader(self.file)
+            numStations = sum(1 for row in stations)+1
 
-    with open('_dataset/london.connections.csv') as file2:
-        connections = csv.reader(file2)
-        for fileLine in connections:
-            #print(fileLine)
-            if connections.line_num != 1:
-                graph.add_edge(int(fileLine[0]), int(fileLine[1]), int(fileLine[3]))
-                
+            graph = Graph(numStations)
+
+            #Have to open it again for some reason
+            with open('_dataset/london.stations.csv') as self.file:
+                stations = csv.reader(self.file)
+                for fileLine in stations:
+                    if stations.line_num != 1:
+                        graph.add_node_dist(float(fileLine[0]), float(fileLine[1]), float(fileLine[2]))
+            with open('_dataset/london.connections.csv') as self.file2:
+                connections = csv.reader(self.file2)
+                for fileLine in connections:
+                    #print(fileLine)
+                    if connections.line_num != 1:
+                        graph.add_edge(int(fileLine[0]), int(fileLine[1]), int(fileLine[3]))
+            return graph
+                    
    #------------------creating graph ---------------------------------     
-    
-    #graph.print_adj_list()
-    #graph.print_node_dist()
+with open('_dataset/london.stations.csv') as file:
+            stations = csv.reader(file)
+
+with open('_dataset/london.connections.csv') as file2:
+                connections = csv.reader(file2)
+
+# app = BuildGraph(file,file2)
+# graph = app.build_graph()
+# graph.print_adj_list()
+# graph.print_node_dist()
 
--- src/Metrics.py
+++ src/Metrics.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from Graph import Graph
+from Graph import BuildGraph
 import csv
 import math
 
@@ -15,31 +16,25 @@ import math
 # graph.add_edge(9, 8, 3)
 # graph.add_edge(6, 0, 2)
 
-#------------------creating graph ---------------------------------
+
+#GRAPH (NODE_DIST) TEST CODE ---------------->
+# graph = Graph(5)
+# graph.add_node_dist(1, 51.5846, -0.2786)
+# graph.add_node_dist(2, 51.4991, -0.1115)
+# graph.add_node_dist(3, 51.5119, -0.1756)
+
+
+
 with open('_dataset/london.stations.csv') as file:
-    stations = csv.reader(file)
+            stations = csv.reader(file)
 
-    # row count other than first line of headers is number of stations
-    numStations = sum(1 for row in stations)+1
-
-    graph = Graph(numStations)
-
-    #Have to open it again for some reason
-    with open('_dataset/london.stations.csv') as filee:
-        stationss = csv.reader(filee)
-        for fileLine in stationss:
-            if stationss.line_num != 1:
-                graph.add_node_dist(float(fileLine[0]), float(fileLine[1]), float(fileLine[2]))
-
-    with open('_dataset/london.connections.csv') as file2:
-        connections = csv.reader(file2)
-        for fileLine in connections:
-            #print(fileLine)
-            if connections.line_num != 1:
-                graph.add_edge(int(fileLine[0]), int(fileLine[1]), int(fileLine[3]))
-#------------------creating graph ---------------------------------
+with open('_dataset/london.connections.csv') as file2:
+                connections = csv.reader(file2)
+app = BuildGraph(file,file2)
+graph = app.build_graph()
 
 adjList = graph.get_adjList()
+nodeDist = graph.get_node_dist()
 
 #--------------- STRATEGY PATTERN ------------------------------------------------------
 #abstract method
@@ -48,6 +43,22 @@ class graphMetricsStrategy(ABC,Graph):
    #call with adj_list from graph
    def create_metric(self, m_adj_list): #add -> to specify return type 
      pass
+
+class nodeDegreeStrategy(graphMetricsStrategy):
+   def create_metric(self, m_adj_list):
+      degOfNodes = []
+      #iterate through each node in the list
+      for node in m_adj_list:
+         degree = 0
+         duplicate = []
+         #check if the connection has already been accounted for (would add a duplicate), otherwise increment degree
+         for tuple in m_adj_list[node]:
+            if tuple[0] in duplicate:
+               continue
+            duplicate.append(tuple[0])
+            degree += 1
+         degOfNodes.append((node,degree))
+      return degOfNodes
 
 
 class numEdgesStrategy(graphMetricsStrategy):
@@ -58,77 +69,62 @@ class numEdgesStrategy(graphMetricsStrategy):
                edges += 1
       return edges
 
-# -----------------may not be able to use in strategy pattern ----------
-# class nodeDistanceStrategy(graphMetricsStrategy):
-#    def create_metric(self, nodeOne, nodeTwo):
-#         node_dist = graph.get_node_dist()
-#         temp = node_dist[nodeOne]
-#         temp2 = node_dist[nodeTwo]
+# may not be able to use in strategy pattern -------------------------->
 
-#         #must be a better way
-#         for i in temp:
-#          nodeOneLong = i[0]
-#          nodeOneLat = i[1]
-#         for j in temp2:
-#          nodeTwoLong = j[0]
-#          nodeTwoLat = j[1]
+class nodeDistanceStrategy(graphMetricsStrategy):
+   def create_metric(self, m_adj_list):
+      return 9
 
-#         a = nodeOneLong - nodeTwoLong
-#         b = nodeOneLat - nodeTwoLat
-#         c = math.sqrt(a**2 + b**2)
-#         print(c)
-#         return c
+   # implementation with nodeOne and nodeTwo
+   # def create_metric(self, nodeOne, nodeTwo):
+   #      node_dist = graph.get_node_dist()
+   #      temp = node_dist[nodeOne]
+   #      temp2 = node_dist[nodeTwo]
 
-# class avgNodeDegStrategy(graphMetricsStrategy):
-#    def create_metric(self, nodeOne, nodeTwo):
+   #      #must be a better way
+   #      for i in temp:
+   #       nodeOneLong = i[0]
+   #       nodeOneLat = i[1]
+   #      for j in temp2:
+   #       nodeTwoLong = j[0]
+   #       nodeTwoLat = j[1]
+
+   #      a = nodeOneLong - nodeTwoLong
+   #      b = nodeOneLat - nodeTwoLat
+   #      c = math.sqrt(a**2 + b**2)
+   #      return c
 
          
       
 
-def process_graph(processingStrategy: graphMetricsStrategy):
-  numEdges = processingStrategy.create_metric(adjList)
-  print(numEdges)
+def process_metrics(processingStrategy: graphMetricsStrategy):
+   metric = processingStrategy.create_metric(adjList)
+   print(metric) 
 
-#process_graph(numEdgesStrategy(1))    #figure out why we need a random parameter
-#process_graph(nodeDistance(1))
+  
+# Run metrics --------->
+#process_metrics(numEdgesStrategy(1))    #figure out why we need a random parameter
+#process_metrics(nodeDistanceStrategy(1))
+#process_metrics(nodeDegreeStrategy(1))
+
+
 
 #--------------- STRATEGY PATTERN ------------------------------------------------------
 
 
-#GRAPH (NODE_DIST) TEST CODE ---------------->
-graph = Graph(5)
-graph.add_node_dist(1, 51.5846, -0.2786)
-graph.add_node_dist(2, 51.4991, -0.1115)
-graph.add_node_dist(3, 51.5119, -0.1756)
 
 
 
-def nodeDistance(nodeOne, nodeTwo):
-   node_dist = graph.get_node_dist()
-   temp = node_dist[nodeOne]
-   temp2 = node_dist[nodeTwo]
-   #must be a better way
-   for i in temp:
-      nodeOneLong = i[0]
-      nodeOneLat = i[1]
-   for j in temp2:
-      nodeTwoLong = j[0]
-      nodeTwoLat = j[1]
-   
-   a = nodeOneLong - nodeTwoLong
-   b = nodeOneLat - nodeTwoLat
-   c = math.sqrt(a**2 + b**2)
-   print(c)
-   return c
 
-   
 
-nodeDistance(1,2)
-   
 
-# nodeDist = graph.get_node_dist()
 
-#nodeDistance(nodeDist, 51.4991, -0.1115)
+
+
+
+
+
+
 
 # -------------------ONE NODE TO EVERY NODE IMPLEMENTATION NODE DISTANCE ---------------------------------------
 # def nodeDistance(node_dist,nodeLong, nodeLat):
@@ -151,9 +147,26 @@ nodeDistance(1,2)
 # -------------------ONE NODE TO EVERY NODE IMPLEMENTATION NODE DISTANCE ---------------------------------------    
 
 
+#------------------creating graph ---------------------------------
+# with open('_dataset/london.stations.csv') as file:
+#     stations = csv.reader(file)
 
+#     # row count other than first line of headers is number of stations
+#     numStations = sum(1 for row in stations)+1
 
+#     graph = Graph(numStations)
 
+#     #Have to open it again for some reason
+#     with open('_dataset/london.stations.csv') as filee:
+#         stationss = csv.reader(filee)
+#         for fileLine in stationss:
+#             if stationss.line_num != 1:
+#                 graph.add_node_dist(float(fileLine[0]), float(fileLine[1]), float(fileLine[2]))
 
-
-
+#     with open('_dataset/london.connections.csv') as file2:
+#         connections = csv.reader(file2)
+#         for fileLine in connections:
+#             #print(fileLine)
+#             if connections.line_num != 1:
+#                 graph.add_edge(int(fileLine[0]), int(fileLine[1]), int(fileLine[3]))
+#------------------creating graph ---------------------------------
